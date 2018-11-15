64eba167f1c2b53808dea8b85326c9b69ef2d3f1
Jesper <[jp222vq@student.lnu.se]>
2018-11-15 17:35:11 +0100

    updated maven to alpine and renamed docker containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to DSR</h2>
+          <h2>Welcome to ParkingZpot</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
