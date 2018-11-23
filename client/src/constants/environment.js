da2aee3b2bfc70fce23fb1a62ca05a9dddaf9e4f
Jesper <[jp222vq@student.lnu.se]>
2018-11-23 12:53:45 +0100

    now able to add car from client

--- client/src/constants/environment.js
+++ client/src/constants/environment.js
@@ -2,7 +2,7 @@
 export const API_ROOT = '/api';
 export const AUTH_URI = `${API_ROOT}/auth/login`;
 export const REGISTER_URI = `${API_ROOT}/auth/register`;
-export const VEHICLE_URI = `${API_ROOT}/auth/vehicles`;
+export const VEHICLE_URI = `${API_ROOT}/vehicles`;
 
 // Modals
 export const LOGIN_MODAL = 'showLoginModal';
