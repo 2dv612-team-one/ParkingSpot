9ca529420e2252d37bd562c7abc05d31f66f5a73
Alex <alex.naga@hotmail.com>
2018-11-27 16:05:38 +0100

    Changed logo to .svg

--- client/src/components/MenuBar/MenuBar.js
+++ client/src/components/MenuBar/MenuBar.js
@@ -7,7 +7,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import logo from '../../assets/images/parkingzpot_white.png';
+import logo from '../../assets/images/parkingzpot_white.svg';
 
 import { logout } from '../../actions/authenticate';
 import { openModal } from '../../actions/modal';
