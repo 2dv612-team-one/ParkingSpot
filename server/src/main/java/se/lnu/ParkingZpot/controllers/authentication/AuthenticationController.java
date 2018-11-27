a3e7200ed085edc40fe99def156e9df99081867f
Daniel Anbarestani <Daniel.Anbarestani@iCloud.com>
2018-11-27 12:27:38 +0100

    Add token validation end point and functionality

--- server/src/main/java/se/lnu/ParkingZpot/controllers/authentication/AuthenticationController.java
+++ server/src/main/java/se/lnu/ParkingZpot/controllers/authentication/AuthenticationController.java
@@ -8,10 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import se.lnu.ParkingZpot.exceptions.ApplicationException;
 import se.lnu.ParkingZpot.models.Role;
@@ -51,6 +48,18 @@ public class AuthenticationController {
 
     @Autowired
     private EmailService emailService;
+
+    @GetMapping("/validate")
+    public ResponseEntity validateToken(@RequestParam("token") String authToken) {
+      boolean validToken = tokenProvider.validateToken(authToken);
+
+      if (validToken) {
+        return new ResponseEntity(new ApiResponse(true, "Token is valid"), HttpStatus.OK);
+      } else {
+        return new ResponseEntity(new ApiResponse(false, "Token is invalid"), HttpStatus.FORBIDDEN);
+      }
+
+    }
 
     @PostMapping("/login")
     public ResponseEntity authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
