82cd7c73d870ab2f5df24eed1920880e0dbb6bbb
Alex <alex.naga@hotmail.com>
2018-12-18 14:02:00 +0100

    Refresh message list

--- client/src/components/SnackbarHandling/SnackbarHandling.js
+++ client/src/components/SnackbarHandling/SnackbarHandling.js
@@ -23,11 +23,21 @@ const mapDispatchToProps = dispatch => ({
 class SnackbarHandling extends Component {
   state = {
     viewed: [],
+    clickCount: 0,
   };
 
   storeViewed = (id) => {
+    const { viewed } = this.state;
+    if (viewed.includes(id)) return;
+
     this.setState(({ viewed }) => ({
       viewed: [...viewed, id],
+    }));
+  };
+
+  increaseClickCount = () => {
+    this.setState(({ clickCount }) => ({
+      clickCount: clickCount += 1,
     }));
   };
 
@@ -38,27 +48,38 @@ class SnackbarHandling extends Component {
 
   handleViewed = (id) => {
     const { accessToken, markMessageViewed, removeSnackbar } = this.props;
+    const { viewed } = this.state;
     markMessageViewed(id, accessToken);
     removeSnackbar(id);
-  };
+    this.increaseClickCount();
+  }
 
   componentDidUpdate(prevProps) {
-    const { infoMessages, successMessages, errorMessages, enqueueSnackbar } = this.props;
-    const { viewed } = this.state;
+    const { infoMessages, successMessages, errorMessages, enqueueSnackbar, accessToken, getUnseenMessages } = this.props;
+    const { viewed, clickCount } = this.state;
 
     // let allMessages = infoMessages.concat(successMessages, errorMessages);
-    // console.log("__ALL MESSAGES__HERE__");
-    // console.log(infoMessages);
 
     if (infoMessages !== prevProps.infoMessages) {
+
+      if (clickCount === 3) {
+        getUnseenMessages(accessToken);
+
+        this.setState(({ viewed }) => ({
+          viewed: [],
+        }));
+        this.setState(({ clickCount }) => ({
+          clickCount: 0,
+        }));
+      }
+
       infoMessages.forEach((message) => {
-        if (message === undefined) {
-          return;
-        }
-        const timeInSeconds = 600 * 1000;
+        if (message === undefined) return;
+
+        const timeInSeconds = 600;
         const options = {
           variant: "info",
-          autoHideDuration: timeInSeconds,
+          autoHideDuration: timeInSeconds * 1000,
           action: <IconButton key="close" aria-label="Close" color="inherit" onClick={() => this.handleViewed(message.id)}><CheckIcon /></IconButton>,
         };
         setTimeout(() => {
@@ -75,37 +96,35 @@ class SnackbarHandling extends Component {
     }
 
     errorMessages.forEach((message) => {
-      if (message === undefined) {
-        return;
-      }
-      const timeInSeconds = 6 * 1000;
+      if (message === undefined) return;
+
+      const timeInSeconds = 6;
       const options = {
         variant: "error",
-        autoHideDuration: timeInSeconds,
+        autoHideDuration: timeInSeconds * 1000,
         action: <IconButton key="close" aria-label="Close" color="inherit" onClick={() => this.handleClose(message.id)}><CheckIcon /></IconButton>,
       };
       setTimeout(() => {
-        let messageSeen = viewed.indexOf(message.id) > -1;
+        let messageSeen = viewed.includes(message.id);
         if (messageSeen) return;
 
         // Display message using notistack
         if (typeof enqueueSnackbar === "function") { enqueueSnackbar(message.message, options); }
-        this.handleClose(message.id);
+        // this.handleClose(message.id);
       }, 1);
     })
 
     successMessages.forEach((message) => {
-      if (message === undefined) {
-        return;
-      }
-      const timeInSeconds = 6 * 1000;
+      if (message === undefined) return;
+
+      const timeInSeconds = 6;
       const options = {
         variant: "success",
-        autoHideDuration: timeInSeconds,
+        autoHideDuration: timeInSeconds * 1000,
         action: <IconButton key="close" aria-label="Close" color="inherit" onClick={() => this.handleClose(message.id)}><CheckIcon /></IconButton>,
       };
       setTimeout(() => {
-        let messageSeen = viewed.indexOf(message.id) > -1;
+        let messageSeen = viewed.includes(message.id);
         if (messageSeen) return;
 
         // Display message using notistack
@@ -116,7 +135,7 @@ class SnackbarHandling extends Component {
     })
   }
 
-  render() { return <div></div>; }
+  render() { return null; }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(withSnackbar(SnackbarHandling));
